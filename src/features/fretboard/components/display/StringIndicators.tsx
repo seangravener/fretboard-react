2b63b2224fc53f12c7ae68568825a92907210d67
Sean Gravener <sean@gravener.net>
2025-01-19 19:19:28 -0500

    Show string note and muted\open state

--- src/features/fretboard/components/display/StringIndicators.tsx
+++ src/features/fretboard/components/display/StringIndicators.tsx
@@ -3,15 +3,10 @@ import { getStringIndicator } from "../../utils/fretboard.utils";
 
 type Props = {
   fretboard: Fretboard;
-  displayNotes?: boolean;
   onFretClick?: (stringNum: StringNumber, fretNum: FretNumber) => void;
 };
 
-export const StringIndicators = ({
-  fretboard,
-  displayNotes,
-  onFretClick,
-}: Props) => (
+export const StringIndicators = ({ fretboard, onFretClick }: Props) => (
   <div className="string-indicators" aria-label="String Indicators">
     {fretboard.strings.map((string) => (
       <button
@@ -19,7 +14,10 @@ export const StringIndicators = ({
         className="string-indicator"
         data-string-number={string.stringNumber}
         onClick={() => onFretClick?.(string.stringNumber, 0)}
-        title={displayNotes ? string.openNote : ""}
+        title={
+          (string.frets[0].isHighlighted ? `Open` : `Muted`) +
+          ` (${string.openNote})`
+        }
       >
         {getStringIndicator(string)}
       </button>
